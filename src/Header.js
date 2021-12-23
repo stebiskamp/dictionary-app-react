fb76f7b732ba8bae48f788409ddeab7259e22182
stebiskamp <86976063+stebiskamp@users.noreply.github.com>
2021-12-23 20:34:02 +0100

    Integrate Pexels API to my Search Engine Component

--- src/Header.js
+++ src/Header.js
@@ -3,17 +3,18 @@ import "./Header.css";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Header(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Header text-capitalize">
         {props.results.word}
-        <span className="Phonetic"> {props.results.phonetic}</span>
-        <ReactAudioPlayer
-          src={props.results.phonetics[0].audio}
-          autoPlay={false}
-          controls
-        />
+        <span className="Phonetic"> {props.results.phonetics[0].text}</span>
+        <div className="Player">
+          <ReactAudioPlayer
+            src={props.results.phonetics[0].audio}
+            autoPlay={false}
+            controls
+          />
+        </div>
       </div>
     );
   } else {
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -7,12 +7,19 @@ import "./SearchEngine.css";
 export default function SearchEngine() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [pexelsResults, setPexelsResults] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     //URL for documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey = `563492ad6f91700001000001a1505f7d1d3045c99b278af6ba29611c`;
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1"`;
+    axios
+      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
+      .then(handlePexelsResponse);
   }
 
   function handleChange(event) {
@@ -21,8 +28,12 @@ export default function SearchEngine() {
   }
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPexelsResults(response.data);
   }
 
   return (
