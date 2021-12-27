c60e01b42e2edb9902c83ff59b6f74a8f53cf88e
stebiskamp <86976063+stebiskamp@users.noreply.github.com>
2021-12-27 20:22:36 +0100

    Improved CSS from Header and App.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> What word are you looking for?</h1>
+        <h1> World's most amazing dictionary</h1>
+        <h2> What word are you looking for?</h2>
         <header className="App-header">
           <SearchEngine />
         </header>
